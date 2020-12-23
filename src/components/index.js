3966cc441de757db27707c72b332d46d2d2f79c4
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2020-12-23 12:51:51 +0500

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Cards }
+    from './Cards/Cards'
+export { default as Chart }
+    from './Chart/Chart'
+export { default as CountryPicker }
+    from './CountryPicker/CountryPicker'
+
+//new way to export so we can import in just oneline
